f38b93076554229c81911f308548b7845b0bcdef
conorheins <conor.heins@gmail.com>
2024-08-06 14:25:33 +0200

    began RGM class implementation Co-authored-by: Tim Verbelen <tverbele@users.noreply.github.com> Co-authored-by: Alexander Tschantz <alec-tschantz@users.noreply.github.com>

---
+++ examples/structure_learning/rgm.py
@@ -0,0 +1,16 @@
+
+
+class RGM:
+
+
+    def infer_states(self, observations):
+        pass
+
+    def infer_empirical_prior(self, path, qs):
+        pass
+
+    def infer_policies(self, qs):
+        pass
+
+    def sample_action(self, q_pi):
+        pass
